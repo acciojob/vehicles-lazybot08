5126e60ecb90a9569aa7193e445378f66df9189c
Aryan Jaiswal <aryanj868@gmai.com>
2024-08-25 08:46:16 +0530

    Last commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -90,9 +90,7 @@ public class Car extends Vehicle {
 
     public void changeSpeed(int newSpeed, int newDirection){
     	
-    	this.setCurrentSpeed(this.getCurrentSpeed()+newSpeed);
-    	
-    	this.setCurrentDirection(this.getCurrentDirection()+newDirection);
+    	this.move(newSpeed, newDirection);
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
